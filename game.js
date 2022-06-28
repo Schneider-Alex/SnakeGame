77da5d193dbd5a940b37e69d3be9c0e5d5ca04fc
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-28 14:45:32 -0400

    game is totally functional, just needs styling

--- game.js
+++ game.js
@@ -1,10 +1,18 @@
-import {SNAKE_SPEED, update as updateSnake, draw as drawSnake} from "./snake.js"
-
+import {SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection} from "./snake.js"
+import {update as updateFood, draw as drawFood} from "./food.js"
+import {outsideGrid} from "./grid.js"
 let lastRenderTime = 0
+let gameOver=false
 const gameBoard = document.getElementById("gameboard")
 
 
 function main(currentTime){
+    if (gameOver){
+        if(confirm('You Lost. Press ok to restart')){
+            window.location='/'
+        }
+        return
+    }
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime)/1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED ) return
@@ -16,10 +24,16 @@ window.requestAnimationFrame(main)
 
 function update(){
     updateSnake()
-
+    updateFood()
+    checkDeath()
 }
 
 function draw(){
     gameBoard.innerHTML=""
     drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
--- snake.js
+++ snake.js
@@ -1,24 +1,58 @@
-export const SNAKE_SPEED = 3
-const snakeBody = [{x:10,y:11},{x:11,y:11},{x:12,y:11}]
+import { getInputDirection } from "./input.js"
 
+export const SNAKE_SPEED = 5
+const snakeBody = [{ x: 11, y: 11 }]
+let newSegments = 0
 
-export function update(){
-    for (let i =  snakeBody.length -2; i>=0; i--){
-        snakeBody[i+1] = {...snakeBody[i]}
+export function update() {
+    addSegments()
+    const inputDirection = getInputDirection()
+    for (let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i] }
     }
-    snakeBody[0].x +=0
-    snakeBody[0].y+=1
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
 
     console.log('update snake')
 }
 
-export function draw(gameBoard){
-    snakeBody.forEach(segment=>{
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div')
         snakeElement.style.gridRowStart = segment.y
-        snakeElement.style.gridColumnStart= segment.x
+        snakeElement.style.gridColumnStart = segment.x
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
     })
     console.log('draw Snake')
+}
+
+export function expandSnake(amount) {
+    newSegments += amount
+}
+
+export function onSnake(position, {ignoreHead = false}={}) {
+    return snakeBody.some((segment, index) => {
+        if (ignoreHead && index==0) return false
+        return eqaulPositions(segment, position)
+    })
+}
+
+function eqaulPositions(pos1, pos2) {
+    return (pos1.x === pos2.x && pos1.y === pos2.y)
+}
+
+function addSegments(){
+    for (let i=0; i<newSegments; i++){
+        snakeBody.push ({...snakeBody[snakeBody.length-1]})
+    }
+    newSegments =0
+}
+
+export function getSnakeHead(){
+    return snakeBody[0]
+}
+
+export function snakeIntersection(){
+    return onSnake(snakeBody[0], {ignoreHead:true})
 }
